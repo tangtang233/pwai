af361741f780a793258fc6efa80f6b329cb03e88
tangtang233 <toby911115@163.com>
2018-02-08 23:31:49 +0800

    第一次代码上传。采用SpringBoot生成的项目。练习用。

---
+++ src/main/java/com/thww/pwai/PwaiApplication.java
@@ -0,0 +1,12 @@
+package com.thww.pwai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwaiApplication.class, args);
+	}
+}
